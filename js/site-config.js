6581eb98516970f7dbb573aec57b7270b7c90779
sosorke <sosorwvk@gmail.com>
2026-04-06 20:13:34 -0300

    Update site-config.js

--- js/site-config.js
+++ js/site-config.js
@@ -1,1 +1,4 @@
-
+window.CLINICA_CONFIG = {
+  googleAppsScriptUrl: "https://script.google.com/macros/s/AKfycbyPLePmFTdcxU82TJ2M0RAiS_buU3Y4xqxP7qgM6unYvgT__RDkA8A4ZweINKLoGGxfyQ/exec",
+  googleSheetUrl: ""
+};
